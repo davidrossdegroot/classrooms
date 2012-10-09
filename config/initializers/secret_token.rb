d81f8ac750acaaddefd78f024418986496f62e72
David De Groot <daviddegroot@David-De-Groots-MacBook-Air>
2012-10-09 16:01:35 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classrooms::Application.config.secret_token = '049fa2dd1e3930a938c63a12d4d8e2d767929039cf5bac110bccf69d5694f23bf53623ad952b31d0405dc26c4c992042be366dc120083b65ec4852be2c0a6e1d'

de330e3ebfce8e4007ff7e038c30b297b9eff720
David De Groot <daviddegroot@David-De-Groots-MacBook-Air>
2012-10-14 01:45:33 -0400

    removed the config.action_mailer from environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Classrooms::Application.initialize!
 
-  config.action_mailer.default_url_options = { :host => 'stark-chamber-3648.herokuapp.com' }
-

c141ca8e73eec78b192055821fd0721241ba9147
David De Groot <daviddegroot@David-De-Groots-MacBook-Air>
2012-10-14 01:08:45 -0400

    updated suggestion model

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -2,8 +2,8 @@ class Suggestion < ActiveRecord::Base
   has_many :users
   attr_accessible :suggestion_id, :name, :instructor, :description
   
-  protected
-  def add_user_to_class
-    current_user.suggestions.build(params[:suggestions])
-  end
+  #protected
+  #def add_user_to_class
+  #  current_user.suggestions.build(params[:suggestions])
+  #end
 end

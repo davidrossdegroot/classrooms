1890260c5cc174ed96c4fa059fbbbadd7a013d46
David De Groot <daviddegroot@David-De-Groots-MacBook-Air>
2012-10-14 00:46:47 -0400

    updated environment.rb for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Classrooms::Application.initialize!
+
+  config.action_mailer.default_url_options = { :host => 'stark-chamber-3648.herokuapp.com' }
+

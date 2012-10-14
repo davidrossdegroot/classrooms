bb8b12a1059c62d3a41923e314932f12b9eb463c
David De Groot <daviddegroot@David-De-Groots-MacBook-Air>
2012-10-14 09:02:31 -0400

    added a script for pushing changes to github and heroku

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Add all changed files to git local change repo
+echo adding files to git
+git add .
+
+# Commit all
+echo committing all changed files to local repo 
+git commit  # Write a commit message for the file.
+ # It might be a good idea to use a format like "filename: changes"
+
+# Push to github and heroku
+echo Pushing to heroku and github
+git push heroku master --app stark-chamber-3648
+git push origin master
